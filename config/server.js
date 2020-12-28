d5db1f7be0f3d52fd3ecb83231ef736239887710
ramy <ramy.niranjan@gmail.com>
2020-12-28 22:44:44 +0100

    make it better

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9ba276b632121bd645df98b9d652848c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd880f31f-1bac-4403-8fc9-88f42d97f858'
+};
